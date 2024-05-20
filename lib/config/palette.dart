c26afb91488a2f390683a5be773770a9302c9086
Mays abu kishik <110351326+mayskishik@users.noreply.github.com>
2024-05-20 10:00:53 +0300

    Add files via upload

---
+++ lib/config/palette.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color scaffold = Color(0xFFF0F2F5);
+
+  static const Color REDcolor = const Color.fromRGBO(226, 124, 126, 0.978);
+
+  static const LinearGradient createRoomGradient = LinearGradient(
+    colors: [const Color.fromRGBO(226, 124, 126, 0.978), Color(0xFFCE48B1)],
+  );
+
+  static const Color online = Color(0xFF4BCB1F);
+
+  static const LinearGradient storyGradient = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [Colors.transparent, Colors.black26],
+  );
+}
