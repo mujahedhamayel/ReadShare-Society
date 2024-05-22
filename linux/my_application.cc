2b402c9a6086bab6d2ec2ac2db572f828e1aca34
mujahedhamayel <91940567+mujahedhamayel@users.noreply.github.com>
2024-05-22 20:31:30 +0300

    auth

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "bookv_app");
+    gtk_header_bar_set_title(header_bar, "read_share");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "bookv_app");
+    gtk_window_set_title(window, "read_share");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
