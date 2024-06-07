489f37f49d4212d0cbbce08bf676be54246304d7
mays ismael <mays.kishik@gmail.com>
2024-06-07 03:53:19 +0300

    add favoritve icon

--- lib/screens/favoritePage.dart
+++ lib/screens/favoritePage.dart
@@ -21,7 +21,8 @@ class _FavoritePageState extends State<FavoritePage> {
     super.initState();
     Provider.of<BookProvider>(context, listen: false).fetchLikedBooks();
   }
-@override
+
+  @override
   Widget build(BuildContext context) {
     final bookProvider = Provider.of<BookProvider>(context);
 
@@ -44,76 +45,77 @@ class _FavoritePageState extends State<FavoritePage> {
         child: Container(
           constraints: const BoxConstraints(maxWidth: 400),
           child: bookProvider.likedBooks.isEmpty
-            ? const Text('No books available')
-            : ListView.builder(
-              itemCount: bookProvider.likedBooks.length,
-              itemBuilder: (BuildContext context, int index) {
-                final book = bookProvider.likedBooks[index];
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailPage(book: book),
+              ? const Text('No books available')
+              : ListView.builder(
+                  itemCount: bookProvider.likedBooks.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final book = bookProvider.likedBooks[index];
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailPage(book: book),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        height: 136,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: const Color(0xFFE0E0E0)),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    book.title,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    "${book.author} · ${book.updateDate.toLocal()}",
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Icon(Icons.bookmark, size: 16),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(8.0),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(book.imgUrl),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
-                  child: Container(
-                    height: 136,
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 16, vertical: 8.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: const Color(0xFFE0E0E0)),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    padding: const EdgeInsets.all(8),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                book.title,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold),
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              const SizedBox(height: 8),
-                              Text(
-                                "${book.author} · ${book.updateDate.toLocal()}",
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-                              const SizedBox(height: 8),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Icon(Icons.bookmark_border_rounded, size: 16),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                        Container(
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(8.0),
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(book.imgUrl),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
+                ),
         ),
       ),
     );
